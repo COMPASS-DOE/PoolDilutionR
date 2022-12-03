cd291b2effcebfe17d6b9dd25c94d47661bc796e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-12-03 14:50:13 -0500

    Add pdr_fractionation internal dataset

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' P and k fractionation values
+#'
+#' A compendium of possible production (P) and consumption (k) fractionation
+#' values, by gas.
+#'
+#' @format
+#' \describe{
+#'   \item{Gas}{Gas}
+#'   \item{frac_P}{Fractionation value of production (P)}
+#'   \item{frac_k}{Fractionation value of consumption (k)}
+#'   \item{Default}{Default for this gas? Logical}
+#'   \item{Source}{Source paper or URL}
+#' }
+"pdr_fractionation"
